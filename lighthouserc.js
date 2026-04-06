3a6918de3a956999feec5c11273db00189bdf10a
Boban Pepic <boban@sellingdubai.com>
2026-04-06 06:17:32 +0200

    ci: tighten Lighthouse CI assertions and add job timeout

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,16 +5,15 @@ module.exports = {
         'https://sellingdubai.ae/',
         'https://sellingdubai.ae/landing.html',
       ],
-      numberOfRuns: 1,
+      numberOfRuns: 3,
     },
     assert: {
       assertions: {
         'categories:performance': ['error', { minScore: 0.8 }],
-        'first-contentful-paint': ['warn', { maxNumericValue: 3000 }],
+        'first-contentful-paint': ['error', { maxNumericValue: 2000 }],
         'largest-contentful-paint': ['error', { maxNumericValue: 2500 }],
-        'total-blocking-time': ['warn', { maxNumericValue: 300 }],
+        'total-blocking-time': ['error', { maxNumericValue: 200 }],
         'cumulative-layout-shift': ['error', { maxNumericValue: 0.1 }],
-        'interactive': ['warn', { maxNumericValue: 5000 }],
       },
     },
     upload: {
